d07a93c6f236158bc125d072a0ef9d379ffa1151
Mahesh1094 <mfunde1010@gmail.com>
2025-09-02 13:56:03 +0000

    changes added

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "maheshfunde@gmail.com";
+var accountPassword = "12345";
+
+accountEmail = "abcd@gmail.com";
+accountCity = "Pune";
+
+accountPassword = "54321";
+console.log(accountId);
+accountCity = "Mumbai";
+console.log(accountEmail);
+console.log(accountCity);
+console.table({ accountId, accountEmail, accountPassword, accountCity });
